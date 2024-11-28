635c2bcdff3f9f5e66ccd899d289c0614cc5fad5
Yurei <69757457+Yurei21@users.noreply.github.com>
2024-11-28 20:31:23 +0800

    Added Landing Page with functional logging in and signing up

--- Frontend/src/Router.jsx
+++ Frontend/src/Router.jsx
@@ -4,11 +4,13 @@ import Register from './partials/register/register';
 import Records from './partials/records/records';
 import Recovery from './partials/recovery/recovery';
 import Home from './pages/home/home';
+import Landing from './partials/landing/landing.jsx';
 
 function RouterApp() {
 	return (
 		<Routes>
-			<Route path="/" element={<Navigate to="/login" replace />} />
+			<Route path="/" element={<Navigate to="/landing" replace />} />
+			<Route path='/landing' element={<Landing />} />
 			<Route path="/login" element={<Login />} />
 			<Route path='/register' element={<Register/>} />
 			<Route path='/home/*' element={<Home/>} />
---
+++ Frontend/src/partials/profile editing/profileEdit.jsx
@@ -0,0 +1,5 @@
+import { useContext } from "react";
+import { SessionContext } from "../home/home";
+import Loading from "../../partials/loading/loading";
+import style from './profileEdit.module.css'
+import Loading from "../loading/loading";
