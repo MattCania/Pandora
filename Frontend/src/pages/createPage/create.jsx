9eb2d31d7b1e72d3a33d8c362657546416e23f30
Yurei <69757457+Yurei21@users.noreply.github.com>
2024-12-04 21:12:17 +0800

    Create module finish but missing link for

--- Frontend/src/pages/createPage/create.jsx
+++ Frontend/src/pages/createPage/create.jsx
@@ -1,20 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
+import { faFolderOpen, faBox, faPlus} from "@fortawesome/free-solid-svg-icons";
+import styles from './create.module.css';
+import { Link } from 'react-router-dom';
 
 function Create () {
 
 	return(
 		<section >
-			<div>
-				Record
-			</div>
-			<div>
-				Transaction
-			</div>
-			<div>
-				Inventory
-			</div>
-
+			<body className={styles.body}>
+				<div className={styles.button}>
+					<Link to='/home/records/create'>
+					<button>
+						<FontAwesomeIcon icon={faFolderOpen} size="2x" />
+						<span>Record</span>
+					</button>
+					</Link>
+					<Link to='/home/records'>
+					<button>
+						<FontAwesomeIcon icon={faPlus} size="2x" />
+						<span>Transaction</span>
+					</button>
+					</Link>
+					<button>
+						<FontAwesomeIcon icon={faBox} size="2x" />
+						<span>Inventory</span>
+					</button>
+				</div>
+			</body>
 		</section>
 	)
 
