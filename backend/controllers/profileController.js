522cf5e62696f88f2aa422250826ab08f2b4c294
Matthew Cania <139668141+MattCania@users.noreply.github.com>
2024-12-05 02:51:43 +0800

    Idk anymore

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -24,25 +24,54 @@ const getUserDetails = async (req, res) =>{
 }
 
 const updateProfile = async (req, res) => {
-	const profileId = req.session.userId
+	const userId = req.session.userId
 	const {
-		userName, contactNumber,
-		secondaryEmail, country,
-		jobTitle, organization,
-		department, street,
-		city, state,
-		postal, birthday,
-		gender
-	} = req.body
+		userName = "",
+		email = "",
+		suffix = "",
+		firstName = "",
+		lastName = "",
+		middleName = "",
+		contactNumber = "",
+		birthday = "",
+		gender = "",
+		jobTitle = "",
+		organization = "",
+		department = "",
+		backupEmail = "",
+		street = "",
+		city = "",
+		state = "",
+		postal = "",
+		country = ""
+	} = req.body;
 
 	try {
+
+		const accountUpdate = await UserAccounts.update(
+			{
+				firstName: firstName,
+				lastName: lastName,
+				middleName: middleName,
+				suffix: suffix,
+				email: email,
+			},
+			{
+				where: {
+					userId: userId,
+				},
+			}
+		);
+
+		if (!accountUpdate.ok) res.status(500).json({message: "Error Account Update"})
+
 		const response = await UserProfiles.update({
 			where: {
-				profileId: profileId
+				profileId: userId
 			},
 			userName: userName,
 			contactNumber: contactNumber || null,
-			secondaryEmail: secondaryEmail || null,
+			secondaryEmail: backupEmail || null,
 			country: country || null,
 			jobTitle: jobTitle || null,
 			organization: organization || null,
@@ -57,7 +86,7 @@ const updateProfile = async (req, res) => {
 
 		if (!response.ok) throw new Error("Error Updating Profile")
 
-		res.status(500).json({message: "Successfully Updated Profile"})
+		res.status(200).json({message: "Successfully Updated Profile"})
 	} catch (err) {
 		return res.status(500).json({ message: "Server Error", error: err.message });
 	}
