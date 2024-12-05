fd9be9a745ce749566e385d0d424b9bafe2c6541
Yurei <69757457+Yurei21@users.noreply.github.com>
2024-12-06 03:06:34 +0800

    responsivess

--- Frontend/src/partials/landing/about.jsx
+++ Frontend/src/partials/landing/about.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from "react";
-import styles from "./landing.module.css"
+import styles from "./about.module.css"
 import Logo from '/src/assets/MainLogo.svg';
 import { Link } from "react-router-dom";
 
@@ -25,10 +25,8 @@ function about() {
             </div>
             <div className={styles.lowerSection}>
                 <div className={styles.halfLeft}>
-                    <div className={styles.mainText}>
-                        <h1>About Us</h1>
-                    </div>
                     <div className={styles.paragraph}>
+                        <h1>About Us</h1>
                         <h3>At Pandora, we are passionate about helping individuals and businesses achieve financial clarity and confidence through precise, reliable, and tailored bookkeeping solutions. Our mission is to simplify the complexities of financial management, providing you with the tools and support you need to make informed decisions and focus on what truly matters—growing your business and achieving your goals.</h3>
                         <h3>We believe that strong financial foundations are the backbone of success. By partnering with us, you gain more than a bookkeeping service—you gain a trusted ally dedicated to your prosperity. Let us take the burden of financial management off your shoulders so you can focus on what you do best: leading your business to new heights.</h3>
                     </div>
